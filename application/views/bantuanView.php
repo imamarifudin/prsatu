82a82eec91cba96f8392bf5678731b1f633466f1
Imam Arifudin <imam.ar@students.amikom.ac.id>
2021-07-12 13:59:26 +0700

    rev

--- application/views/bantuanView.php
+++ application/views/bantuanView.php
@@ -1,6 +1,6 @@
 <div class="section-tittle text-center mb-80">
 	<h2>Bantuan</h2>
-	<h2>Penggunaan Terhadap Peta</h2>
+	<h2>Penggunaan Peta</h2>
 	<br><br>
 	<div class="row justify-content-md-center">
 		<img src="<?= templates('') ?>">
